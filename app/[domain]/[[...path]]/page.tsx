98a15dd0bd08ae9d658d3a2ec96e36ab5d5f506c
Richard Hagen <richard.hagen@gmail.com>
2025-04-11 22:38:30 +0200

    Nav to root work correctly

--- app/[domain]/[[...path]]/page.tsx
+++ app/[domain]/[[...path]]/page.tsx
@@ -16,6 +16,8 @@ async function Home(props: Props) {
       <h1>Msg: {data.hello}</h1>
       <h2>Domain: {data.domain}</h2>
       <h3>Path: {data.path}</h3>
+      <Link href={"/"}>Navigating to Root works</Link>
+      <br/><br/>
       <Link href={"/some-page"}>Some page</Link>
       <br/><br/>
       <Link href={"/test/some-page"}>Some Sub page is also broken</Link>
